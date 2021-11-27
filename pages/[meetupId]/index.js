4e5d29c447425c0188d80189c84f9fa1927fae7f
Rahul Ajmani <rahulajmani910@gmail.com>
2021-11-27 16:59:08 +0530

    adding meetup details and getstaticprops

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import MeetupDetail from '../../components/meetups/MeetupDetails'
+
+const MeetupDetails = () => {
+    return (
+        <>
+            <MeetupDetail
+                image='https://images.news18.com/ibnlive/uploads/2020/02/Donald-Trump-at-Taj-Mahal.jpg?impolicy=website&width=0&height=0'
+                title='A First Meetup'
+                address='Taj Mahal, Agra, India'
+                description='The meetup description'
+            />
+        </>
+    )
+}
+
+export default MeetupDetails
